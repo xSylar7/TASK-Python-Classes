ed4dd66a9d03217ff1daf8a6cdd417e71ff4bc8b
Qassem Alsaffar <qas.alsaffar@gmail.com>
2022-10-04 17:58:59 +0300

    Finished the task

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 class Wallet:
-    def __init__(self, money):
+    #  money = 0 The default value of money is 0
+    def __init__(self, money=0):
         self.money = money
 
     def credit(self, amount):
@@ -9,12 +10,17 @@ class Wallet:
     def debit(self, amount):
         self.money = amount - self.money
         print (f"Your debit amount is {self.money}")
+    
+    def __str__(self):
+        return f"{self.money}"
 
 
-wallet = Wallet(6)
+wallet = Wallet(15)
 # wallet = Wallet(0)  # This should default money inside the wallet to 0
 wallet.credit(5)
-print(wallet.money)
+print(wallet)
+wallet.debit(5)
+print(wallet)
 
 
 
@@ -23,11 +29,14 @@ class Person:
     def __init__(self, name, location,money):
         self.name = name
         self.location = location
-        self.money = Wallet(money)
+        self.wallet = Wallet(money)
 
     def move_to(self, point):
-        self.location == point 
-        print (f"Your name is {self.name} and have {self.money} money and moved from {self.location} location to {point}")
+        self.location = point 
+        print (f"Your name is {self.name} and have {self.wallet} money and moved from {self.location} location to {point}")
+    
+    def __str__(self):
+        return f"Your name is {self.name}, your location {self.location}, your money {self.wallet.money}"
 
 person = Person("Qassem", 5, 50)
 print (person.move_to(10))
@@ -35,25 +44,51 @@ print (person.move_to(10))
 
 class Vendor (Person):
     # implement Vendor!
-    def __init__(self, name, location, money, range, price):
+    def __init__(self, name, location, money, range = 5, price = 1):
         super().__init__(name, location, money)
-        self.range = range.max (5)
-        self.price = price + 1
+        self.range = range
+        self.price = price
     
     def sell_to (self, customer, number_of_icecreams):
-        self.customer = customer
-        self.number_of_icecreams = number_of_icecreams
-        self.move_to(customer,location)
-        self.money.credit(self.price * number_of_icecreams)
+        self.location = customer.location
+        self.wallet.credit(self.price * number_of_icecreams)
+        customer.wallet.debit (self.price * number_of_icecreams)
+        print (f"{number_of_icecreams} ice creams were sold")
         
 
 
-# vendor = Vendor("Abdallah", 3, 6)
+vendor = Vendor("Abdallah",3 ,6)
 
 
-class Customer:
+class Customer (Person):
     # implement Customer!
-    pass
+    def __init__(self, name, location, money):
+        super().__init__(name, location, money)
+        
+    
+    def is_in_range (self,vendor):
+        distance = abs(self.location - vendor.location)
+        if distance <= vendor.range:
+            print(f"This vendor {vendor.name} within {self.name}")
+            return True
+        else:
+            print (f"This vendor {vendor.name} is not within {self.name}")
+            return False
+        
+    def have_enough_money (self, vendor, number_icecreams):
+        if self.wallet.money >= vendor.price * number_icecreams:
+            print ("You have enough money")
+            return True
+            
+        else:
+            print ("You dont have enough money")
+            return False
 
+    def request_icecream (self, vendor, number_icecream):
+        if self.is_in_range(vendor) and self.have_enough_money(vendor,number_icecream):
+            Vendor.sell_to(self,number_icecream)
 
-# customer = Customer("Abdallah", 3, 6)
+vendor_one = Vendor ("Talal", 3 ,60)
+customer_one = Customer("Abbas", 80, 6)
+
+customer_one._is_in_range(vendor_one)
